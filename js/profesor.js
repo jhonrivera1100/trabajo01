27c4450d492b0a03528d69ae93d8f76bf305bf68
jhonrivera1100 <jhonrivera416@gmail.com>
2023-06-23 13:12:46 -0500

    manufernandez

---
+++ js/profesor.js
@@ -0,0 +1,7 @@
+const roles=parseInt(prompt( "introduzca su rol 1=estudiante , 2=profesor , 3=administrador")) ;
+
+if (roles==2){
+    const postularme=document.querySelector(".postularme");
+    postularme.textContent="postularme";    
+
+}
